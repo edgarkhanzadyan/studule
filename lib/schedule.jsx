c4294bae77be8c5595fb29cffb6da973ba1d9ce4
edgarkhanzadyan <edgar.khanzadyan@yandex.ru>
2016-11-15 18:49:31 +0300

    init_commit

---
+++ lib/schedule.jsx
@@ -0,0 +1,33 @@
+const React = require('react');
+const { Component } = React;
+const style = require('./style');
+class Schedule extends Component {
+  render() {
+    return(
+      <div>
+        <header>
+          <div style={style.logo}>
+            Studule
+          </div>
+        </header>
+        <div style={style.scheduleAdd}>
+          <div style={style.inputs}>
+            <input style={style.classInput} />
+            <input style={style.whenClassInput} />
+            <input style={style.homeworkInput} />
+          </div>
+            <button style={style.buttonDate}></button>
+        </div>
+        <div style={style.mainSchedule}>
+          <div style={style.timeSchedule}>
+
+          </div>
+          <div style={style.homeworkTab}>
+
+          </div>
+        </div>
+      </div>
+    );
+  };
+}
+module.exports = Schedule;
