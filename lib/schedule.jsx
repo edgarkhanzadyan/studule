29d6841d489a60d1330c09e83ba627c3d8409504
edgarkhanzadyan <edgar.khanzadyan@yandex.ru>
2016-11-17 19:15:22 +0300

    express-ws

--- lib/schedule.jsx
+++ lib/schedule.jsx
@@ -5,19 +5,57 @@ class Schedule extends Component {
   constructor(){
     super();
     this.state = {
-    week: [
-    {day:'Monday',schedule: [{id:0, event: 'Freshman'},{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7},{id:8},{id:9},{id:10},{id:11},{id:12},{id:13},{id:14},{id:15},{id:16},{id:17},{id:18},{id:19},{id:20},{id:21},{id:22},{id:23}]},
-    {day:'Tuesday',schedule: [{id:0},{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7},{id:8},{id:9},{id:10},{id:11},{id:12},{id:13},{id:14},{id:15},{id:16},{id:17},{id:18},{id:19},{id:20},{id:21},{id:22},{id:23}]},
-    {day:'Wendnsday',schedule: [{id:0},{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7},{id:8},{id:9},{id:10},{id:11},{id:12},{id:13},{id:14},{id:15},{id:16},{id:17},{id:18},{id:19},{id:20},{id:21},{id:22},{id:23}]},
-    {day:'Thursday',schedule: [{id:0},{id:1},{id:2},{id:3},{id:4, event: 'hey'},{id:5},{id:6},{id:7},{id:8},{id:9},{id:10},{id:11},{id:12},{id:13},{id:14},{id:15},{id:16},{id:17},{id:18},{id:19},{id:20},{id:21},{id:22},{id:23}]},
-    {day:'Friday',schedule: [{id:0},{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7},{id:8},{id:9},{id:10},{id:11},{id:12},{id:13},{id:14},{id:15},{id:16},{id:17},{id:18},{id:19},{id:20},{id:21},{id:22},{id:23}]},
-    {day:'Saturday',schedule: [{id:0},{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7},{id:8},{id:9},{id:10},{id:11},{id:12},{id:13},{id:14},{id:15},{id:16},{id:17},{id:18},{id:19},{id:20},{id:21},{id:22},{id:23}]},
-    {day:'Sunday',schedule: [{id:0},{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7},{id:8},{id:9},{id:10},{id:11},{id:12},{id:13},{id:14},{id:15},{id:16},{id:17},{id:18},{id:19},{id:20},{id:21},{id:22},{id:23}]}],
-    bool: [false,false,false,true,false,false,false]
+      week: [
+      {day:'Monday',schedule: [{id:0, event: 'Freshman'},{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7},{id:8},{id:9},{id:10},{id:11},{id:12},{id:13},{id:14},{id:15},{id:16},{id:17},{id:18},{id:19},{id:20},{id:21},{id:22},{id:23}]},
+      {day:'Tuesday',schedule: [{id:0},{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7},{id:8},{id:9},{id:10},{id:11},{id:12},{id:13},{id:14},{id:15},{id:16},{id:17},{id:18},{id:19},{id:20},{id:21},{id:22},{id:23}]},
+      {day:'Wendnsday',schedule: [{id:0},{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7},{id:8},{id:9},{id:10},{id:11},{id:12},{id:13},{id:14},{id:15},{id:16},{id:17},{id:18},{id:19},{id:20},{id:21},{id:22},{id:23}]},
+      {day:'Thursday',schedule: [{id:0},{id:1},{id:2},{id:3},{id:4, event: 'hey'},{id:5},{id:6},{id:7},{id:8},{id:9},{id:10},{id:11},{id:12},{id:13},{id:14},{id:15},{id:16},{id:17},{id:18},{id:19},{id:20},{id:21},{id:22},{id:23}]},
+      {day:'Friday',schedule: [{id:0},{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7},{id:8},{id:9},{id:10},{id:11},{id:12},{id:13},{id:14},{id:15},{id:16},{id:17},{id:18},{id:19},{id:20},{id:21},{id:22},{id:23}]},
+      {day:'Saturday',schedule: [{id:0},{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7},{id:8},{id:9},{id:10},{id:11},{id:12},{id:13},{id:14},{id:15},{id:16},{id:17},{id:18},{id:19},{id:20},{id:21},{id:22},{id:23}]},
+      {day:'Sunday',schedule: [{id:0},{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7},{id:8},{id:9},{id:10},{id:11},{id:12},{id:13},{id:14},{id:15},{id:16},{id:17},{id:18},{id:19},{id:20},{id:21},{id:22},{id:23}]}],
+      bool: [false,false,false,true,false,false,false],
+      bufClass: '',
+      bufTime: '',
+      bufHomework: '',
+      bufDay: ''
     };
+    this.conn = new WebSocket('ws://localhost:4000');
+    this.onDayChange = this.onDayChange.bind(this);
+    this.onTimeChange = this.onTimeChange.bind(this);
+    this.onClassChange = this.onClassChange.bind(this);
+    this.onHomeworkChange = this.onHomeworkChange.bind(this);
+    this.clickHandlerPushEvent = this.clickHandlerPushEvent.bind(this);
   }
-  clickHandler(e){
-
+  sendData((data) => this.conn.send(JSON.stringify({
+    cmd: 'new_data',
+    payload: data,
+  })));
+  clickHandlerPushEvent(e){
+    if((e.button === 0 || e.key === 'Enter') && this.state.bufClass.trim() !== '' && this.state.bufTime.trim() !== ''){
+      const buffer = JSON.stringify({
+        classo : bufClass,
+        time : bufTime,
+        homework : bufHomework,
+      });
+      this.sendData(buffer);
+      this.setState({ ...this.state, bufClass: '', bufTime: '', bufHomework: ''});
+    }
+  }
+  onDayChange(e){
+    const day = e.currentTarget.value;
+    this.setState({...this.state, bufDay: day});
+  }
+  onClassChange(e){
+    const class = e.currentTarget.value;
+    this.setState({...this.state, bufClass: class });
+  }
+  onTimeChange(e){
+    const time = e.currentTarget.value;
+    this.setState({...this.state, bufTime: time });
+  }
+  onHomeworkChange(e){
+    const homework = e.currentTarget.value;
+    this.setState({...this.state, bufHomework: homework });
   }
   render() {
     const renderStyle = {
@@ -63,9 +101,10 @@ class Schedule extends Component {
         </header>
         <div style={style.scheduleAdd}>
           <div style={style.inputs}>
-            <input style={style.classInput} placeholder={'name of the class'} />
-            <input style={style.whenClassInput} placeholder={'when is that class'} />
-            <input style={style.homeworkInput} placeholder={'homework for it'} />
+            <input style={style.classInput} placeholder={'name of the class*'} onChange={this.onClassChange}/>
+            <input style={style.whenClassInput} placeholder={'time of that class*'} onChange={this.onTimeChange}/>
+            <input style={style.whenClassInput} placeholder={'day of that class*'} onChange={this.onDayChange}/>
+            <input style={style.homeworkInput} placeholder={'homework for it'} onChange={this.onHomeworkChange}/>
           </div>
             <button style={style.buttonDate}>put new date!</button>
         </div>
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const React = require('react');
 const ReactDOMServer = require('react-dom/server');
 const Application = require('./lib/schedule');
 const port = 4000;
-
+const expressWs = require('express-ws')(app);
 const { createElement } = React;
 
 const site = `
@@ -25,6 +25,15 @@ const site = `
     </body>
   </html>
 `;
+const schedule = {
+  {day:'Monday',schedule: [{id:0},{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7},{id:8},{id:9},{id:10},{id:11},{id:12},{id:13},{id:14},{id:15},{id:16},{id:17},{id:18},{id:19},{id:20},{id:21},{id:22},{id:23}]},
+  {day:'Tuesday',schedule: [{id:0},{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7},{id:8},{id:9},{id:10},{id:11},{id:12},{id:13},{id:14},{id:15},{id:16},{id:17},{id:18},{id:19},{id:20},{id:21},{id:22},{id:23}]},
+  {day:'Wendnsday',schedule: [{id:0},{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7},{id:8},{id:9},{id:10},{id:11},{id:12},{id:13},{id:14},{id:15},{id:16},{id:17},{id:18},{id:19},{id:20},{id:21},{id:22},{id:23}]},
+  {day:'Thursday',schedule: [{id:0},{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7},{id:8},{id:9},{id:10},{id:11},{id:12},{id:13},{id:14},{id:15},{id:16},{id:17},{id:18},{id:19},{id:20},{id:21},{id:22},{id:23}]},
+  {day:'Friday',schedule: [{id:0},{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7},{id:8},{id:9},{id:10},{id:11},{id:12},{id:13},{id:14},{id:15},{id:16},{id:17},{id:18},{id:19},{id:20},{id:21},{id:22},{id:23}]},
+  {day:'Saturday',schedule: [{id:0},{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7},{id:8},{id:9},{id:10},{id:11},{id:12},{id:13},{id:14},{id:15},{id:16},{id:17},{id:18},{id:19},{id:20},{id:21},{id:22},{id:23}]},
+  {day:'Sunday',schedule: [{id:0},{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7},{id:8},{id:9},{id:10},{id:11},{id:12},{id:13},{id:14},{id:15},{id:16},{id:17},{id:18},{id:19},{id:20},{id:21},{id:22},{id:23}]}],
+}
 
 app.use(express.static('public'));
 
@@ -36,5 +45,15 @@ app.get('/new_schedule', (req, res) => {
   res.setHeader('content-type', 'text/html');
   res.end(main_schedule);
 })
+app.ws('/', (ws, req) => {
+  ws.on('message', (msg) => {
+    const reply = JSON.parse(msg);
 
+    switch (reply.cmd) {
+      case 'new_data':
+        
+    }
+  });
+  console.log('socket', req.testing);
+});
 app.listen(port, () => console.log('started %d', port));
